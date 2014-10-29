ImpalaToGo/ImpalaToGo

Clean up expr substitution and cloning.
Before: The pre- and postconditions of expr substitution and cloning,
in particular, their effect on the isAnalyzed_ flag were unclear and
sometimes inconsistent e.g., some literal exprs set isAnalyzed_ to
true in their c'tor. As a result, several places required ad-hoc
solutions like Expr.unsetIsAnalyzed() and Expr.reanalyze().

This patch cleans up expr substitution and cloning, summarized as follows:

Expr analysis:
All exprs start our with isAnalyzed_ = false. The flag it set to true
iff analyze() has been called on the expr.

Expr.clone():
Creates a deep copy of an expr including all its analysis state.

Expr.equals():
Comparison of expr trees ignores implicit casts. This simplifies expr
substitution because un/analyzed exprs can be easily compared/substituted.

ExprSubstitutionMap:
When adding a mapping, the rhs expr must be analyzed to allow
substitution across query blocks. There is no requirement on the lhs expr.

Expr substitution:
Substitution returns an analyzed clone of the original expr with exprs
substituted. While performing the substitution, implicit casts and analysis
state are removed such that the returned result has minimal implicit casts
and types.
There are two versions of substitute functions: One that throws exceptions
one that does not, because the caller may have different expectations on
whether a substitution must succeed or not.

Numeric literals:
This patch combines IntLiteral and DecimalLiteral into a NumericLiteral.
Its main benefit is that analyze() always produces the same type, even if
the literal was implicitly cast and/or isAnalyzed was unset because of
expr substitution. This was not the case before because an implicit cast
could permanently turn an IntLiteral into a DecimalLiteral.

There is no more need for unsetIsAnalyzed() or reanalyze().

Change-Id: I646110e3714cff8ae8d5a378c25a107dd43334b6
Reviewed-on: http://gerrit.ent.cloudera.com:8080/3228
Reviewed-by: Alex Behm <alex.behm@cloude

fe/src/main/java/com/cloudera/impala/analysis/FunctionCallExpr.java
@@ -37,6 +37,11 @@ public class FunctionCallExpr extends Expr {
   private final FunctionName fnName_;
   private final FunctionParams params_;
 
+  // Indicates whether this is a merge aggregation function. This flag affects
+  // resetAnalysisState() which is used during expr substitution.
+  // TODO: Re-think how we generate the merge aggregation and remove this flag.
+  private boolean isMergeAggFn_ = false;
+
   public FunctionCallExpr(String functionName, List<Expr> params) {
     this(new FunctionName(functionName), new FunctionParams(false, params));
   }
@@ -47,7 +52,7 @@ public class FunctionCallExpr extends Expr {
 
   public FunctionCallExpr(FunctionName fnName, FunctionParams params) {
     super();
-    this.fnName_ = fnName;
+    fnName_ = fnName;
     params_ = params;
     if (params.exprs() != null) children_.addAll(params.exprs());
   }
@@ -72,6 +77,27 @@ public class FunctionCallExpr extends Expr {
     params_ = params;
     // Just inherit the function object from 'e'.
     if (params.exprs() != null) children_.addAll(params.exprs());
+  }
+
+  /**
+   * Copy c'tor used in clone().
+   */
+  protected FunctionCallExpr(FunctionCallExpr other) {
+    super(other);
+    fnName_ = other.fnName_;
+    isMergeAggFn_ = other.isMergeAggFn_;
+    // No need to deep clone the params, its exprs are already in children_.
+    params_ = other.params_;
+  }
+
+  @Override
+  public void resetAnalysisState() {
+    isAnalyzed_ = false;
+    // Resolving merge agg functions after substitution may fail e.g., if the
+    // intermediate agg type is not the same as the output type. Preserve the original
+    // fn_ such that analyze() hits the special-case code for merge agg fns that
+    // handles this case.
+    if (!isMergeAggFn_) fn_ = null;
   }
 
   @Override
@@ -119,6 +145,7 @@ public class FunctionCallExpr extends Expr {
     return params_.isDistinct();
   }
 
+  public void setIsMergeAggFn() { isMergeAggFn_ = true; }
   public FunctionName getFnName() { return fnName_; }
 
   @Override
@@ -223,9 +250,9 @@ public class FunctionCallExpr extends Expr {
           throw new AnalysisException(fnName_.getFunction() +
               "() must be called with a constant second argument.");
         }
-        IntLiteral scaleLiteral = (IntLiteral)LiteralExpr.create(
+        NumericLiteral scaleLiteral = (NumericLiteral) LiteralExpr.create(
             children_.get(1), analyzer.getQueryCtx());
-        resultScale = (int)scaleLiteral.getValue();
+        resultScale = (int)scaleLiteral.getLongValue();
         if (Math.abs(resultScale) > ColumnType.MAX_SCALE) {
           throw new AnalysisException("Cannot round/truncate to scales greater than " +
               ColumnType.MAX_SCALE + ".");
@@ -355,4 +382,7 @@ public class FunctionCallExpr extends Expr {
       type_ = resolveDecimalReturnType(analyzer);
     }
   }
+
+  @Override
+  public Expr clone() { return new FunctionCallExpr(this); }
 }

cloudera/Impala

Reviewed-on: http://gerrit.ent.cloudera.com:8080/3318
(cherry picked from commit a691cb4a2bc3b59e47b20f7f2449561ce53d9aa2)
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5003
Reviewed-by: Juan Yu <jyu@cloudera.com>
Tested-by: Juan Yu <jyu@cloudera.com>

fe/src/main/java/com/cloudera/impala/analysis/FunctionCallExpr.java
@@ -37,6 +37,11 @@ public class FunctionCallExpr extends Expr {
   private final FunctionName fnName_;
   private final FunctionParams params_;
 
+  // Indicates whether this is a merge aggregation function. This flag affects
+  // resetAnalysisState() which is used during expr substitution.
+  // TODO: Re-think how we generate the merge aggregation and remove this flag.
+  private boolean isMergeAggFn_ = false;
+
   public FunctionCallExpr(String functionName, List<Expr> params) {
     this(new FunctionName(functionName), new FunctionParams(false, params));
   }
@@ -47,7 +52,7 @@ public class FunctionCallExpr extends Expr {
 
   public FunctionCallExpr(FunctionName fnName, FunctionParams params) {
     super();
-    this.fnName_ = fnName;
+    fnName_ = fnName;
     params_ = params;
     if (params.exprs() != null) children_.addAll(params.exprs());
   }
@@ -72,6 +77,27 @@ public class FunctionCallExpr extends Expr {
     params_ = params;
     // Just inherit the function object from 'e'.
     if (params.exprs() != null) children_.addAll(params.exprs());
+  }
+
+  /**
+   * Copy c'tor used in clone().
+   */
+  protected FunctionCallExpr(FunctionCallExpr other) {
+    super(other);
+    fnName_ = other.fnName_;
+    isMergeAggFn_ = other.isMergeAggFn_;
+    // No need to deep clone the params, its exprs are already in children_.
+    params_ = other.params_;
+  }
+
+  @Override
+  public void resetAnalysisState() {
+    isAnalyzed_ = false;
+    // Resolving merge agg functions after substitution may fail e.g., if the
+    // intermediate agg type is not the same as the output type. Preserve the original
+    // fn_ such that analyze() hits the special-case code for merge agg fns that
+    // handles this case.
+    if (!isMergeAggFn_) fn_ = null;
   }
 
   @Override
@@ -119,6 +145,7 @@ public class FunctionCallExpr extends Expr {
     return params_.isDistinct();
   }
 
+  public void setIsMergeAggFn() { isMergeAggFn_ = true; }
   public FunctionName getFnName() { return fnName_; }
 
   @Override
@@ -227,9 +254,9 @@ public class FunctionCallExpr extends Expr {
           throw new AnalysisException(fnName_.getFunction() +
               "() must be called with a constant second argument.");
         }
-        IntLiteral scaleLiteral = (IntLiteral)LiteralExpr.create(
+        NumericLiteral scaleLiteral = (NumericLiteral) LiteralExpr.create(
             children_.get(1), analyzer.getQueryCtx());
-        digitsAfter = (int)scaleLiteral.getValue();
+        digitsAfter = (int)scaleLiteral.getLongValue();
         if (Math.abs(digitsAfter) > ColumnType.MAX_SCALE) {
           throw new AnalysisException("Cannot round/truncate to scales greater than " +
               ColumnType.MAX_SCALE + ".");
@@ -357,4 +384,7 @@ public class FunctionCallExpr extends Expr {
       type_ = resolveDecimalReturnType(analyzer);
     }
   }
+
+  @Override
+  public Expr clone() { return new FunctionCallExpr(this); }
 }