ibmsoe/ImpalaPPC

IMPALA-1658: Add compatibility flag for Hive-Parquet-Timestamps
No changes to writing were made. No changes to reading Impala written
files were made.

Hive writes TIMESTAMP values to parquet files differently than Impala
does. Hive converts the value from local time to UTC before writing;
Impala does not. This change adds a startup flag that will convert UTC
to local when reading files written by Hive.

The Hive-file detection actually checks for "parquet-mr" (which is the
library Hive uses) in the file metadata. A slight possibility exists
that TIMESTAMP values written by something other than Hive but also
using parquet-mr may become incorrect. The possibility should be very
small because TIMESTAMP values are stored and encoded in a non-standard
way other applications are unlikely to be aware of.

Flags from be/src/exec/hdfs-parquet-scanner.cc:
  -convert_legacy_hive_parquet_utc_timestamps (When true, TIMESTAMPs
    read from files written by Parquet-MR (used by Hive) will be
    converted from UTC to local time. Writes are unaffected.) type: bool
    default: false

Change-Id: I79a499fe24049b7025ee2dd76c9c3e07010d346a
Reviewed-on: http://gerrit.cloudera.org:8080/35
Reviewed-by: Casey Ching <casey@cloudera.com>
Tested-by: Internal Jenkins

be/src/runtime/timestamp-value.cc
@@ -37,6 +37,27 @@ int TimestampValue::Format(const DateTimeFormatContext& dt_ctx, int len, char* b
   return TimestampParser::Format(dt_ctx, date_, time_, len, buff);
 }
 
+void TimestampValue::UtcToLocal() {
+  DCHECK(HasDateAndTime());
+  try {
+    tm temp_tm = to_tm(ptime(date_, time_));  // will throw if date/time is invalid
+    time_t utc = timegm(&temp_tm);
+    if (UNLIKELY(NULL == localtime_r(&utc, &temp_tm))) {
+      *this = ptime(not_a_date_time);
+      return;
+    }
+    // Unlikely but a time zone conversion may push the value over the min/max
+    // boundary resulting in an exception.
+    ptime local = ptime_from_tm(temp_tm);
+    // Neither time_t nor struct tm allow fractional seconds so they have to be handled
+    // separately.
+    local += nanoseconds(time_.fractional_seconds());
+    *this = local;
+  } catch (std::exception& from_boost) {
+    *this = ptime(not_a_date_time);
+  }
+}
+
 ostream& operator<<(ostream& os, const TimestampValue& timestamp_value) {
   return os << timestamp_value.DebugString();
 }

cloudera/Impala

(cherry picked from commit 256ef1c1481a24c0d76e3e48ee1d3410f6dc760a)
(cherry picked from commit dd533e847b0a9e89e391bbbefd43f49a298190df)
(cherry picked from commit 6f94b4ca1c37a120480d13bcdc38e49e7a15b08f)
(cherry picked from commit feb34784316efea9aa1d268d35f1076ca55e8583)

be/src/runtime/timestamp-value.cc
@@ -47,6 +47,27 @@ int TimestampValue::Format(const DateTimeFormatContext& dt_ctx, int len, char* b
   return TimestampParser::Format(dt_ctx, date_, time_of_day_, len, buff);
 }
 
+void TimestampValue::UtcToLocal() {
+  DCHECK(HasDateAndTime());
+  try {
+    tm temp_tm = to_tm(ptime(date_, time_of_day_));  // will throw if date/time_of_day is invalid
+    time_t utc = timegm(&temp_tm);
+    if (UNLIKELY(NULL == localtime_r(&utc, &temp_tm))) {
+      *this = ptime(not_a_date_time);
+      return;
+    }
+    // Unlikely but a time zone conversion may push the value over the min/max
+    // boundary resulting in an exception.
+    ptime local = ptime_from_tm(temp_tm);
+    // Neither time_t nor struct tm allow fractional seconds so they have to be handled
+    // separately.
+    local += nanoseconds(time_of_day_.fractional_seconds());
+    *this = local;
+  } catch (std::exception& from_boost) {
+    *this = ptime(not_a_date_time);
+  }
+}
+
 ostream& operator<<(ostream& os, const TimestampValue& timestamp_value) {
   return os << timestamp_value.DebugString();
 }