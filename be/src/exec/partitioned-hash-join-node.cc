82b8ee45782adb3d0222b9e7ea652b4b68445899
Ippokratis Pandis <ipandis@cloudera.com>
2015-04-03 14:14:48 -0700

    IMPALA-1919: Avoid calling ProcessBatch with out_batch->AtCapacity in right joins
    PHJ::GetNext() of RIGHT_OUTER, RIGHT_ANTI and FULL_OUTER joins that
    had repartitioned were not checking whether the output batch reached
    capacity at the OutputUnmatchedBuild() call. In case of repartitioned
    joins where the list of build_partitions was exhausted and the output
    batch has already reached capacity, we would call ProcessProbeBatch()
    with a full output batch, resulting a DCHECK. This patch adds the
    missing AtCapacity() check.
    
    It also adds a new join test (tpch-out-joins) that uses the TPC-H
    dataset and moves there some of the join tests that were using it.
    Running join tests with the larger TPC-H dataset is needed, for
    example, in order to trigger repartitions.
    
    Change-Id: I4434ad0683e1b09f75a25b3eb870a817d4988370
    Reviewed-on: http://gerrit.cloudera.org:8080/314
    Reviewed-by: Ippokratis Pandis <ipandis@cloudera.com>
    Tested-by: Internal Jenkins
    (cherry picked from commit 58baf64fc343f4dd8cc0b35b59e62ac40806a599)
    (cherry picked from commit fc8ecd49e20aef2b50311d11e9bcf334abad1834)

--- be/src/exec/partitioned-hash-join-node.cc
+++ be/src/exec/partitioned-hash-join-node.cc
@@ -752,6 +752,7 @@ Status PartitionedHashJoinNode::GetNext(RuntimeState* state, RowBatch* out_batch
         *eos = true;
         break;
       }
+      if (out_batch->AtCapacity()) break;
     }
 
     if (join_op_ == TJoinOp::NULL_AWARE_LEFT_ANTI_JOIN) {
@@ -763,21 +764,21 @@ Status PartitionedHashJoinNode::GetNext(RuntimeState* state, RowBatch* out_batch
 
       if (null_probe_output_idx_ >= 0) {
         RETURN_IF_ERROR(OutputNullAwareNullProbe(state, out_batch));
-        if (out_batch->AtCapacity()) return Status::OK;
+        if (out_batch->AtCapacity()) break;
         continue;
       }
 
       if (nulls_build_batch_.get() != NULL) {
         RETURN_IF_ERROR(OutputNullAwareProbeRows(state, out_batch));
-        if (out_batch->AtCapacity()) return Status::OK;
+        if (out_batch->AtCapacity()) break;
         continue;
       }
     }
 
     // Finish up the current batch.
     {
-      // Putting SCOPED_TIMER in ProcessProbeBatch() causes weird exception handling IR in
-      // the xcompiled function, so call it here instead.
+      // Putting SCOPED_TIMER in ProcessProbeBatch() causes weird exception handling IR
+      // in the xcompiled function, so call it here instead.
       int rows_added = 0;
       SCOPED_TIMER(probe_timer_);
       if (process_probe_batch_fn_ == NULL || ht_ctx_->level() != 0) {
