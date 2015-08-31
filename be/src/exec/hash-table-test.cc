a87eaa025e3c6c0c4995491b494602a8546ac9e8
Tim Armstrong <tarmstrong@cloudera.com>
2015-08-31 14:05:12 -0700

    IMPALA-2270: avoid FnvHash64to32 with empty inputs
    FnvHash64to32 produces pathologically bad results when hashing zero-byte
    input: it always returns 0 regardless of the input hash seed. This
    is a result of it xoring the 32-bit hash seed with itself. This patch
    adds a DCHECK to this function to verify that this function is not
    invoked with zero-byte inputs, and updates all callsites to check for
    the zero-length case.
    
    This patch also improves hashing of booleans: false and NULL no longer
    hash to the same value.
    
    Change-Id: I6706f6ea167e5362d55351f7cc0c637c680a315d
    Reviewed-on: http://gerrit.cloudera.org:8080/720
    Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
    Tested-by: Internal Jenkins
    (cherry picked from commit 8ed5130e1d83243491ad4d787921495fa17f864f)

--- be/src/exec/hash-table-test.cc
+++ be/src/exec/hash-table-test.cc
@@ -343,6 +343,19 @@ done_inserting:
   mem_pool_.FreeAll();
 }
 
+// Test that hashing empty string updates hash value.
+TEST_F(HashTableTest, HashEmpty) {
+  HashTableCtx ht_ctx(build_expr_ctxs_, probe_expr_ctxs_, false, false, 1, 2, 1);
+  uint32_t seed = 9999;
+  ht_ctx.set_level(0);
+  EXPECT_NE(seed, ht_ctx.Hash(NULL, 0, seed));
+  // TODO: level 0 uses CRC hash, which only swaps bytes around on empty input.
+  // EXPECT_NE(seed, ht_ctx.Hash(NULL, 0, ht_ctx.Hash(NULL, 0, seed)));
+  ht_ctx.set_level(1);
+  EXPECT_NE(seed, ht_ctx.Hash(NULL, 0, seed));
+  EXPECT_NE(seed, ht_ctx.Hash(NULL, 0, ht_ctx.Hash(NULL, 0, seed)));
+}
+
 }
 
 int main(int argc, char** argv) {
