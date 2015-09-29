ibmsoe/ImpalaPPC

IMPALA-2178: fix Expr::ComputeResultsLayout() logic
I am honestly not sure what the original code handling CHAR was
supposed to do, but regardless of the intent I don't think it was
right. This patch rewrites the ComputeResultsLayout() logic to change
the handling of CHAR to reflect my understanding of how the compiler
would layout the memory, as well make the rest of the logic easier to
understand.

Change-Id: I4dfc5968c35dc9fc81bb87926a04410d335205b6
Reviewed-on: http://gerrit.cloudera.org:8080/617
Reviewed-by: Skye Wanderman-Milne <skye@cloudera.com>
Tested-by: Internal Jenkins

be/src/exprs/expr-test.cc
@@ -4329,15 +4329,40 @@ TEST_F(ExprTest, ResultsLayoutTest) {
   ValidateLayout(exprs, 0, -1, expected_offsets);
 
   // Test single Expr case
+  vector<ColumnType> types;
+  types.push_back(TYPE_BOOLEAN);
+  types.push_back(TYPE_TINYINT);
+  types.push_back(TYPE_SMALLINT);
+  types.push_back(TYPE_INT);
+  types.push_back(TYPE_BIGINT);
+  types.push_back(TYPE_FLOAT);
+  types.push_back(TYPE_DOUBLE);
+  types.push_back(TYPE_TIMESTAMP);
+  types.push_back(TYPE_STRING);
+
+  types.push_back(ColumnType::CreateDecimalType(1,0));
+  types.push_back(ColumnType::CreateDecimalType(8,0));
+  types.push_back(ColumnType::CreateDecimalType(18,0));
+
+  types.push_back(ColumnType::CreateCharType(1));
+  types.push_back(ColumnType::CreateVarcharType(1));
+  types.push_back(ColumnType::CreateCharType(2));
+  types.push_back(ColumnType::CreateVarcharType(2));
+  types.push_back(ColumnType::CreateCharType(3));
+  types.push_back(ColumnType::CreateVarcharType(3));
+  types.push_back(ColumnType::CreateCharType(128));
+  types.push_back(ColumnType::CreateVarcharType(128));
+  types.push_back(ColumnType::CreateCharType(255));
+  types.push_back(ColumnType::CreateVarcharType(255));
+
   expected_offsets.clear();
-  for (int type = TYPE_BOOLEAN; type <= TYPE_STRING; ++type) {
-    ColumnType t = ColumnType(static_cast<PrimitiveType>(type));
+  BOOST_FOREACH(const ColumnType& t, types) {
     exprs.clear();
     expected_offsets.clear();
     // With one expr, all offsets should be 0.
     expected_offsets[t.GetByteSize()] = list_of(0);
     exprs.push_back(pool.Add(Literal::CreateLiteral(t, "0")));
-    if (t.type == TYPE_STRING) {
+    if (t.IsVarLenStringType()) {
       ValidateLayout(exprs, 16, 0, expected_offsets);
     } else {
       ValidateLayout(exprs, t.GetByteSize(), -1, expected_offsets);
@@ -4353,7 +4378,7 @@ TEST_F(ExprTest, ResultsLayoutTest) {
   // The expected result is computed along the way
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BOOLEAN, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TINYINT, "0")));
-  exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TINYINT, "0")));
+  exprs.push_back(pool.Add(Literal::CreateLiteral(ColumnType::CreateCharType(1), "0")));
   expected_offsets[1].insert(expected_byte_size);
   expected_offsets[1].insert(expected_byte_size + 1);
   expected_offsets[1].insert(expected_byte_size + 2);
@@ -4361,38 +4386,59 @@ TEST_F(ExprTest, ResultsLayoutTest) {
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_SMALLINT, "0")));
   expected_offsets[2].insert(expected_byte_size);
-  expected_byte_size += 1 * 2 + 2;  // 2 bytes of padding
+  expected_byte_size += 2; // No padding before CHAR
+
+  exprs.push_back(pool.Add(Literal::CreateLiteral(ColumnType::CreateCharType(3), "0")));
+  expected_offsets[3].insert(expected_byte_size);
+  expected_byte_size += 3 + 3; // 3 byte of padding
+  DCHECK_EQ(expected_byte_size % 4, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_INT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_FLOAT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_FLOAT, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(9, 0), "0")));
   expected_offsets[4].insert(expected_byte_size);
   expected_offsets[4].insert(expected_byte_size + 4);
   expected_offsets[4].insert(expected_byte_size + 8);
-  expected_byte_size += 3 * 4 + 4;  // 4 bytes of padding
+  expected_offsets[4].insert(expected_byte_size + 12);
+  expected_byte_size += 4 * 4 + 4;  // 4 bytes of padding
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_DOUBLE, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(18, 0), "0")));
   expected_offsets[8].insert(expected_byte_size);
   expected_offsets[8].insert(expected_byte_size + 8);
   expected_offsets[8].insert(expected_byte_size + 16);
   expected_offsets[8].insert(expected_byte_size + 24);
-  expected_byte_size += 4 * 8;      // No more padding
+  expected_offsets[8].insert(expected_byte_size + 32);
+  expected_byte_size += 5 * 8;      // No more padding
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TIMESTAMP, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TIMESTAMP, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(20, 0), "0")));
   expected_offsets[16].insert(expected_byte_size);
   expected_offsets[16].insert(expected_byte_size + 16);
-  expected_byte_size += 2 * 16;
+  expected_offsets[16].insert(expected_byte_size + 32);
+  expected_byte_size += 3 * 16;
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_STRING, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_STRING, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateVarcharType(1), "0")));
   expected_offsets[0].insert(expected_byte_size);
   expected_offsets[0].insert(expected_byte_size + 16);
+  expected_offsets[0].insert(expected_byte_size + 32);
   expected_var_begin = expected_byte_size;
-  expected_byte_size += 2 * 16;
+  expected_byte_size += 3 * 16;
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   // Validate computed layout
   ValidateLayout(exprs, expected_byte_size, expected_var_begin, expected_offsets);

be/src/exprs/expr.cc
@@ -262,8 +262,11 @@ struct MemLayoutData {
   int expr_idx;
   int byte_size;
   bool variable_length;
+  int alignment;
 
   // TODO: sort by type as well?  Any reason to do this?
+  // TODO: would sorting in reverse order of size be faster due to better packing?
+  // TODO: why put var-len at end?
   bool operator<(const MemLayoutData& rhs) const {
     // variable_len go at end
     if (this->variable_length && !rhs.variable_length) return false;
@@ -279,51 +282,56 @@ int Expr::ComputeResultsLayout(const vector<Expr*>& exprs, vector<int>* offsets,
     return 0;
   }
 
+  // Don't align more than word (8-byte) size. There's no performance gain beyond 8-byte
+  // alignment, and there is a performance gain to keeping the results buffer small. This
+  // is consistent with what compilers do.
+  int MAX_ALIGNMENT = sizeof(int64_t);
+
   vector<MemLayoutData> data;
   data.resize(exprs.size());
 
   // Collect all the byte sizes and sort them
   for (int i = 0; i < exprs.size(); ++i) {
+    DCHECK(!exprs[i]->type().IsComplexType()) << "NYI";
     data[i].expr_idx = i;
-    if (exprs[i]->type().IsVarLenStringType()) {
-      data[i].byte_size = 16;
-      data[i].variable_length = true;
+    data[i].byte_size = exprs[i]->type().GetSlotSize();
+    DCHECK_GT(data[i].byte_size, 0);
+    data[i].variable_length = exprs[i]->type().IsVarLenStringType();
+
+    bool fixed_len_char = exprs[i]->type().type == TYPE_CHAR && !data[i].variable_length;
+
+    // Compute the alignment of this value. Values should be self-aligned for optimal
+    // memory access speed, up to the max alignment (e.g., if this value is an int32_t,
+    // its offset in the buffer should be divisible by sizeof(int32_t)).
+    // TODO: is self-alignment really necessary for perf?
+    if (!fixed_len_char) {
+      data[i].alignment = min(data[i].byte_size, MAX_ALIGNMENT);
     } else {
-      data[i].byte_size = exprs[i]->type().GetByteSize();
-      data[i].variable_length = false;
+      // Fixed-len chars are aligned to a one-byte boundary, as if they were char[],
+      // leaving no padding between them and the previous value.
+      data[i].alignment = 1;
     }
-    DCHECK_NE(data[i].byte_size, 0);
   }
 
   sort(data.begin(), data.end());
 
   // Walk the types and store in a packed aligned layout
-  int max_alignment = sizeof(int64_t);
-  int current_alignment = data[0].byte_size;
   int byte_offset = 0;
 
   offsets->resize(exprs.size());
-  offsets->clear();
   *var_result_begin = -1;
 
   for (int i = 0; i < data.size(); ++i) {
-    DCHECK_GE(data[i].byte_size, current_alignment);
-    // Don't align more than word (8-byte) size.  This is consistent with what compilers
-    // do.
-    if (exprs[data[i].expr_idx]->type().type == TYPE_CHAR &&
-        !exprs[data[i].expr_idx]->type().IsVarLenStringType()) {
-      // CHARs are not padded, to be consistent with complier layouts
-      // aligns the next value on an 8 byte boundary
-      current_alignment = (data[i].byte_size + current_alignment) % max_alignment;
-    } else if (data[i].byte_size != current_alignment &&
-               current_alignment != max_alignment) {
-      byte_offset += data[i].byte_size - current_alignment;
-      current_alignment = min(data[i].byte_size, max_alignment);
-    }
+
+    // Increase byte_offset so data[i] is at the right alignment (i.e. add padding between
+    // this value and the previous).
+    byte_offset = BitUtil::RoundUp(byte_offset, data[i].alignment);
+
     (*offsets)[data[i].expr_idx] = byte_offset;
     if (data[i].variable_length && *var_result_begin == -1) {
       *var_result_begin = byte_offset;
     }
+    DCHECK(!(i == 0 && byte_offset > 0)) << "first value should be at start of layout";
     byte_offset += data[i].byte_size;
   }
 

cloudera/Impala

(cherry picked from commit a1c6a73f6a3b61bbc3c4e27107bec219790fb17b)
Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7839
Reviewed-by: Juan Yu <jyu@cloudera.com>
Tested-by: Juan Yu <jyu@cloudera.com>
(cherry picked from commit e9a59fa41ae1afb819518d118cc803ef719de086)

be/src/exprs/expr-test.cc
@@ -3560,15 +3560,40 @@ TEST_F(ExprTest, ResultsLayoutTest) {
   ValidateLayout(exprs, 0, -1, expected_offsets);
 
   // Test single Expr case
+  vector<ColumnType> types;
+  types.push_back(TYPE_BOOLEAN);
+  types.push_back(TYPE_TINYINT);
+  types.push_back(TYPE_SMALLINT);
+  types.push_back(TYPE_INT);
+  types.push_back(TYPE_BIGINT);
+  types.push_back(TYPE_FLOAT);
+  types.push_back(TYPE_DOUBLE);
+  types.push_back(TYPE_TIMESTAMP);
+  types.push_back(TYPE_STRING);
+
+  types.push_back(ColumnType::CreateDecimalType(1,0));
+  types.push_back(ColumnType::CreateDecimalType(8,0));
+  types.push_back(ColumnType::CreateDecimalType(18,0));
+
+  types.push_back(ColumnType::CreateCharType(1));
+  types.push_back(ColumnType::CreateVarcharType(1));
+  types.push_back(ColumnType::CreateCharType(2));
+  types.push_back(ColumnType::CreateVarcharType(2));
+  types.push_back(ColumnType::CreateCharType(3));
+  types.push_back(ColumnType::CreateVarcharType(3));
+  types.push_back(ColumnType::CreateCharType(128));
+  types.push_back(ColumnType::CreateVarcharType(128));
+  types.push_back(ColumnType::CreateCharType(255));
+  types.push_back(ColumnType::CreateVarcharType(255));
+
   expected_offsets.clear();
-  for (int type = TYPE_BOOLEAN; type <= TYPE_STRING; ++type) {
-    ColumnType t = ColumnType(static_cast<PrimitiveType>(type));
+  BOOST_FOREACH(const ColumnType& t, types) {
     exprs.clear();
     expected_offsets.clear();
     // With one expr, all offsets should be 0.
     expected_offsets[t.GetByteSize()] = list_of(0);
     exprs.push_back(pool.Add(Literal::CreateLiteral(t, "0")));
-    if (t.type == TYPE_STRING) {
+    if (t.IsVarLen()) {
       ValidateLayout(exprs, 16, 0, expected_offsets);
     } else {
       ValidateLayout(exprs, t.GetByteSize(), -1, expected_offsets);
@@ -3584,7 +3609,7 @@ TEST_F(ExprTest, ResultsLayoutTest) {
   // The expected result is computed along the way
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BOOLEAN, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TINYINT, "0")));
-  exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TINYINT, "0")));
+  exprs.push_back(pool.Add(Literal::CreateLiteral(ColumnType::CreateCharType(1), "0")));
   expected_offsets[1].insert(expected_byte_size);
   expected_offsets[1].insert(expected_byte_size + 1);
   expected_offsets[1].insert(expected_byte_size + 2);
@@ -3592,38 +3617,59 @@ TEST_F(ExprTest, ResultsLayoutTest) {
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_SMALLINT, "0")));
   expected_offsets[2].insert(expected_byte_size);
-  expected_byte_size += 1 * 2 + 2;  // 2 bytes of padding
+  expected_byte_size += 2; // No padding before CHAR
+
+  exprs.push_back(pool.Add(Literal::CreateLiteral(ColumnType::CreateCharType(3), "0")));
+  expected_offsets[3].insert(expected_byte_size);
+  expected_byte_size += 3 + 3; // 3 byte of padding
+  DCHECK_EQ(expected_byte_size % 4, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_INT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_FLOAT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_FLOAT, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(9, 0), "0")));
   expected_offsets[4].insert(expected_byte_size);
   expected_offsets[4].insert(expected_byte_size + 4);
   expected_offsets[4].insert(expected_byte_size + 8);
-  expected_byte_size += 3 * 4 + 4;  // 4 bytes of padding
+  expected_offsets[4].insert(expected_byte_size + 12);
+  expected_byte_size += 4 * 4 + 4;  // 4 bytes of padding
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_BIGINT, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_DOUBLE, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(18, 0), "0")));
   expected_offsets[8].insert(expected_byte_size);
   expected_offsets[8].insert(expected_byte_size + 8);
   expected_offsets[8].insert(expected_byte_size + 16);
   expected_offsets[8].insert(expected_byte_size + 24);
-  expected_byte_size += 4 * 8;      // No more padding
+  expected_offsets[8].insert(expected_byte_size + 32);
+  expected_byte_size += 5 * 8;      // No more padding
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TIMESTAMP, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_TIMESTAMP, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateDecimalType(20, 0), "0")));
   expected_offsets[16].insert(expected_byte_size);
   expected_offsets[16].insert(expected_byte_size + 16);
-  expected_byte_size += 2 * 16;
+  expected_offsets[16].insert(expected_byte_size + 32);
+  expected_byte_size += 3 * 16;
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_STRING, "0")));
   exprs.push_back(pool.Add(Literal::CreateLiteral(TYPE_STRING, "0")));
+  exprs.push_back(pool.Add(
+      Literal::CreateLiteral(ColumnType::CreateVarcharType(1), "0")));
   expected_offsets[0].insert(expected_byte_size);
   expected_offsets[0].insert(expected_byte_size + 16);
+  expected_offsets[0].insert(expected_byte_size + 32);
   expected_var_begin = expected_byte_size;
-  expected_byte_size += 2 * 16;
+  expected_byte_size += 3 * 16;
+  DCHECK_EQ(expected_byte_size % 8, 0);
 
   // Validate computed layout
   ValidateLayout(exprs, expected_byte_size, expected_var_begin, expected_offsets);

be/src/exprs/expr.cc
@@ -257,8 +257,11 @@ struct MemLayoutData {
   int expr_idx;
   int byte_size;
   bool variable_length;
+  int alignment;
 
   // TODO: sort by type as well?  Any reason to do this?
+  // TODO: would sorting in reverse order of size be faster due to better packing?
+  // TODO: why put var-len at end?
   bool operator<(const MemLayoutData& rhs) const {
     // variable_len go at end
     if (this->variable_length && !rhs.variable_length) return false;
@@ -274,51 +277,55 @@ int Expr::ComputeResultsLayout(const vector<Expr*>& exprs, vector<int>* offsets,
     return 0;
   }
 
+  // Don't align more than word (8-byte) size. There's no performance gain beyond 8-byte
+  // alignment, and there is a performance gain to keeping the results buffer small. This
+  // is consistent with what compilers do.
+  int MAX_ALIGNMENT = sizeof(int64_t);
+
   vector<MemLayoutData> data;
   data.resize(exprs.size());
 
   // Collect all the byte sizes and sort them
   for (int i = 0; i < exprs.size(); ++i) {
     data[i].expr_idx = i;
-    if (exprs[i]->type().IsVarLen()) {
-      data[i].byte_size = 16;
-      data[i].variable_length = true;
+    data[i].byte_size = exprs[i]->type().GetSlotSize();
+    DCHECK_GT(data[i].byte_size, 0);
+    data[i].variable_length = exprs[i]->type().IsVarLen();
+
+    bool fixed_len_char = exprs[i]->type().type == TYPE_CHAR && !data[i].variable_length;
+
+    // Compute the alignment of this value. Values should be self-aligned for optimal
+    // memory access speed, up to the max alignment (e.g., if this value is an int32_t,
+    // its offset in the buffer should be divisible by sizeof(int32_t)).
+    // TODO: is self-alignment really necessary for perf?
+    if (!fixed_len_char) {
+      data[i].alignment = min(data[i].byte_size, MAX_ALIGNMENT);
     } else {
-      data[i].byte_size = exprs[i]->type().GetByteSize();
-      data[i].variable_length = false;
+      // Fixed-len chars are aligned to a one-byte boundary, as if they were char[],
+      // leaving no padding between them and the previous value.
+      data[i].alignment = 1;
     }
-    DCHECK_NE(data[i].byte_size, 0);
   }
 
   sort(data.begin(), data.end());
 
   // Walk the types and store in a packed aligned layout
-  int max_alignment = sizeof(int64_t);
-  int current_alignment = data[0].byte_size;
   int byte_offset = 0;
 
   offsets->resize(exprs.size());
-  offsets->clear();
   *var_result_begin = -1;
 
   for (int i = 0; i < data.size(); ++i) {
-    DCHECK_GE(data[i].byte_size, current_alignment);
-    // Don't align more than word (8-byte) size.  This is consistent with what compilers
-    // do.
-    if (exprs[data[i].expr_idx]->type().type == TYPE_CHAR &&
-        !exprs[data[i].expr_idx]->type().IsVarLen()) {
-      // CHARs are not padded, to be consistent with complier layouts
-      // aligns the next value on an 8 byte boundary
-      current_alignment = (data[i].byte_size + current_alignment) % max_alignment;
-    } else if (data[i].byte_size != current_alignment &&
-               current_alignment != max_alignment) {
-      byte_offset += data[i].byte_size - current_alignment;
-      current_alignment = min(data[i].byte_size, max_alignment);
-    }
+
+    // Increase byte_offset so data[i] is at the right alignment (i.e. add padding between
+    // this value and the previous).
+    byte_offset = BitUtil::RoundUp(byte_offset, data[i].alignment);
+
     (*offsets)[data[i].expr_idx] = byte_offset;
     if (data[i].variable_length && *var_result_begin == -1) {
       *var_result_begin = byte_offset;
     }
+    DCHECK(!(i == 0 && byte_offset > 0)) << "first value should be at start of layout";
     byte_offset += data[i].byte_size;
   }
 