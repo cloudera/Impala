ibmsoe/ImpalaPPC

Checks for IMPALA-2314 and more accurate calculation of PHJ::LargestSpillPartition
This patch adds dchecks that ensure we push in hash_partitions_
allocated (ie non NULL) partition objects.

It also modifies the calculation of the largest partition at PHJ to
also account for the size of the probe stream. Previously, it was
only looking at the size of the build side.

Change-Id: I9fd9b7a89ac220682d41fbadcd67799169da3ab2
Reviewed-on: http://gerrit.cloudera.org:8080/790
Reviewed-by: Ippokratis Pandis <ipandis@cloudera.com>
Tested-by: Internal Jenkins

be/src/exec/partitioned-aggregation-node.cc
@@ -754,8 +754,10 @@ Status PartitionedAggregationNode::CreateHashPartitions(int level) {
 
   DCHECK(hash_partitions_.empty());
   for (int i = 0; i < PARTITION_FANOUT; ++i) {
-    hash_partitions_.push_back(partition_pool_->Add(new Partition(this, level)));
-    RETURN_IF_ERROR(hash_partitions_[i]->InitStreams());
+    Partition* new_partition = new Partition(this, level);
+    DCHECK(new_partition != NULL);
+    hash_partitions_.push_back(partition_pool_->Add(new_partition));
+    RETURN_IF_ERROR(new_partition->InitStreams());
   }
   DCHECK_GT(state_->block_mgr()->num_reserved_buffers_remaining(block_mgr_client_), 0);
 

be/src/exec/partitioned-hash-join-node.cc
@@ -541,10 +541,10 @@ Status PartitionedHashJoinNode::ProcessBuildInput(RuntimeState* state, int level
   }
 
   for (int i = 0; i < PARTITION_FANOUT; ++i) {
-    hash_partitions_.push_back(partition_pool_->Add(
-        new Partition(state, this, level, using_small_buffers_)));
-    RETURN_IF_ERROR(
-        hash_partitions_[i]->build_rows()->Init(id(), runtime_profile(), true));
+    Partition* new_partition = new Partition(state, this, level, using_small_buffers_);
+    DCHECK(new_partition != NULL);
+    hash_partitions_.push_back(partition_pool_->Add(new_partition));
+    RETURN_IF_ERROR(new_partition->build_rows()->Init(id(), runtime_profile(), true));
 
     // Initialize a buffer for the probe here to make sure why have it if we need it.
     // While this is not strictly necessary (there are some cases where we won't need this
@@ -553,8 +553,7 @@ Status PartitionedHashJoinNode::ProcessBuildInput(RuntimeState* state, int level
     // buffer, there is only a small range of build input sizes where this is beneficial
     // (an IO buffer size). It makes the logic much more complex to enable this
     // optimization.
-    RETURN_IF_ERROR(
-        hash_partitions_[i]->probe_rows()->Init(id(), runtime_profile(), false));
+    RETURN_IF_ERROR(new_partition->probe_rows()->Init(id(), runtime_profile(), false));
   }
   COUNTER_ADD(partitions_created_, PARTITION_FANOUT);
   COUNTER_SET(max_partition_level_, level);
@@ -758,8 +757,10 @@ int64_t PartitionedHashJoinNode::LargestSpilledPartition() const {
   int64_t max_rows = 0;
   for (int i = 0; i < hash_partitions_.size(); ++i) {
     Partition* partition = hash_partitions_[i];
+    DCHECK(partition != NULL) << i << " " << hash_partitions_.size();
     if (partition->is_spilled()) {
       int64_t rows = partition->build_rows()->num_rows();
+      rows += partition->probe_rows()->num_rows();
       if (rows > max_rows) max_rows = rows;
     }
   }

cloudera/Impala

(cherry picked from commit d9c843e11982a8eb11f76fcc2960fb2ec6e1e050)
Reviewed-on: http://gerrit.cloudera.org:8080/1192
Reviewed-by: Juan Yu <jyu@cloudera.com>
Tested-by: Juan Yu <jyu@cloudera.com>

be/src/exec/partitioned-aggregation-node.cc
@@ -722,8 +722,10 @@ Status PartitionedAggregationNode::CreateHashPartitions(int level) {
 
   DCHECK(hash_partitions_.empty());
   for (int i = 0; i < PARTITION_FANOUT; ++i) {
-    hash_partitions_.push_back(state_->obj_pool()->Add(new Partition(this, level)));
-    RETURN_IF_ERROR(hash_partitions_[i]->InitStreams());
+    Partition* new_partition = new Partition(this, level);
+    DCHECK(new_partition != NULL);
+    hash_partitions_.push_back(pool_->Add(new_partition));
+    RETURN_IF_ERROR(new_partition->InitStreams());
   }
   DCHECK_GT(state_->block_mgr()->num_reserved_buffers_remaining(block_mgr_client_), 0);
 

be/src/exec/partitioned-hash-join-node.cc
@@ -511,9 +511,10 @@ Status PartitionedHashJoinNode::ProcessBuildInput(RuntimeState* state, int level
   }
 
   for (int i = 0; i < PARTITION_FANOUT; ++i) {
-    hash_partitions_.push_back(pool_->Add(
-        new Partition(state, this, level, using_small_buffers_)));
-    RETURN_IF_ERROR(hash_partitions_[i]->build_rows()->Init(runtime_profile()));
+    Partition* new_partition = new Partition(state, this, level, using_small_buffers_);
+    DCHECK(new_partition != NULL);
+    hash_partitions_.push_back(pool_->Add(new_partition));
+    RETURN_IF_ERROR(new_partition->build_rows()->Init(runtime_profile(), true));
 
     // Initialize a buffer for the probe here to make sure why have it if we need it.
     // While this is not strictly necessary (there are some cases where we won't need this
@@ -522,7 +523,7 @@ Status PartitionedHashJoinNode::ProcessBuildInput(RuntimeState* state, int level
     // buffer, there is only a small range of build input sizes where this is beneficial
     // (an IO buffer size). It makes the logic much more complex to enable this
     // optimization.
-    RETURN_IF_ERROR(hash_partitions_[i]->probe_rows()->Init(runtime_profile(), false));
+    RETURN_IF_ERROR(new_partition->probe_rows()->Init(runtime_profile(), false));
   }
   COUNTER_ADD(partitions_created_, PARTITION_FANOUT);
   COUNTER_SET(max_partition_level_, level);
@@ -731,8 +732,10 @@ int64_t PartitionedHashJoinNode::LargestSpilledPartition() const {
   int64_t max_rows = 0;
   for (int i = 0; i < hash_partitions_.size(); ++i) {
     Partition* partition = hash_partitions_[i];
+    DCHECK(partition != NULL) << i << " " << hash_partitions_.size();
     if (partition->is_spilled()) {
       int64_t rows = partition->build_rows()->num_rows();
+      rows += partition->probe_rows()->num_rows();
       if (rows > max_rows) max_rows = rows;
     }
   }