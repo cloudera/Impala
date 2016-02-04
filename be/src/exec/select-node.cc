b66f4efc9397ec4a5d1fbcd7a1efb842b3a03072
Alex Behm <alex.behm@cloudera.com>
2016-02-04 10:53:45 -0800

    IMPALA-2926: Fix off-by-one bug in SelectNode::CopyRows().
    The bug was that we were not updating child_row_idx_
    when the output batch was at capacity, leading us to
    double count that last child_row_idx_, and incorrectly
    returning extra rows.
    
    Change-Id: I85b2f1c146861ec7756887b0d2c574365d90233e
    Reviewed-on: http://gerrit.cloudera.org:8080/2044
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins
    (cherry picked from commit ddb8a833cd00d3467eddd5e3b7ece8f59dbef811)
    (cherry picked from commit 5e594761cac6e6fab6ceb89ac2a0b63542983f1b)

--- be/src/exec/select-node.cc
+++ be/src/exec/select-node.cc
@@ -92,12 +92,14 @@ bool SelectNode::CopyRows(RowBatch* output_batch) {
   ExprContext** conjunct_ctxs = &conjunct_ctxs_[0];
   int num_conjunct_ctxs = conjunct_ctxs_.size();
 
-  for (; child_row_idx_ < child_row_batch_->num_rows(); ++child_row_idx_) {
+  while (child_row_idx_ < child_row_batch_->num_rows()) {
     // Add a new row to output_batch
     int dst_row_idx = output_batch->AddRow();
     if (dst_row_idx == RowBatch::INVALID_ROW_INDEX) return true;
     TupleRow* dst_row = output_batch->GetRow(dst_row_idx);
     TupleRow* src_row = child_row_batch_->GetRow(child_row_idx_);
+    // Make sure to increment row idx before returning.
+    ++child_row_idx_;
 
     if (EvalConjuncts(conjunct_ctxs, num_conjunct_ctxs, src_row)) {
       output_batch->CopyRow(src_row, dst_row);
