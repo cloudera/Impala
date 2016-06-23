40571677fc551e3222c56df31075fbcbff1dc64e
Lars Volker <lv@cloudera.com>
2016-06-23 04:39:25 +0200

    IMPALA-3776: fix 'describe formatted' for Avro tables
    For Avro tables the column information in the underlying database of the
    Hive metastore can be different from what is specified in the avro
    schema. HIVE-6308 aimed to improve upon this, but for older tables the
    two don't necessarily align.
    
    There are two possible cases:
    
    1) Hive's underlying database contains a column which is not present in
    the Avro schema file. In this case we encounter a NullPointerException
    in DescribeResultFactory.java#L189 when trying to look up the column in
    the internal table object.
    
    2) The Avro schema contains a column, which is not present in the
    underlying database. In this case the column will not be displayed in
    describe formatted.
    
    In addition to the automatic tests I verified this manually by creating
    an Avro table with an external schema file in Hive. This populated the
    underlying database with the column information. I then either removed
    a column from the Avro schema file (case 1) or cleared the column
    information from the "COLUMNS_V2" table in the underlying database
    (case 2) and verified that the change fixed both cases.
    
    Change-Id: Ieb69d3678e662465d40aee80ba23132ea13871a0
    Reviewed-on: http://gerrit.cloudera.org:8080/4126
    Reviewed-by: Lars Volker <lv@cloudera.com>
    Tested-by: Internal Jenkins
    Reviewed-by: Jim Apple <jbapple@cloudera.com>
    (cherry picked from commit 0e886618e26f8480aa8926918e7f80b7bada3884)
    (cherry picked from commit 5d2f1a2f3c1714a14974270116f01645ce49fdc7)

--- fe/src/main/java/com/cloudera/impala/catalog/Table.java
+++ fe/src/main/java/com/cloudera/impala/catalog/Table.java
@@ -390,10 +390,7 @@ public abstract class Table implements CatalogObject {
    */
   public ArrayList<Column> getColumnsInHiveOrder() {
     ArrayList<Column> columns = Lists.newArrayList(getNonClusteringColumns());
-
-    for (Column column: colsByPos_.subList(0, numClusteringCols_)) {
-      columns.add(column);
-    }
+    columns.addAll(getClusteringColumns());
     return columns;
   }
 
@@ -406,6 +403,13 @@ public abstract class Table implements CatalogObject {
       fields.add(new StructField(col.getName(), col.getType(), col.getComment()));
     }
     return new StructType(fields);
+  }
+
+  /**
+   * Returns the list of all partition columns.
+   */
+  public List<Column> getClusteringColumns() {
+    return colsByPos_.subList(0, numClusteringCols_);
   }
 
   /**
