f402ff859f20681e26c6b0400e0808a699b34345
Alex Behm <alex.behm@cloudera.com>
2016-07-08 14:24:04 -0700

    IMPALA-3678: Fix migration of predicates into union operands with an order by + limit.
    There were two separate issues:
    
    First, the SortNode incorrectly picked up unassigned conjuncts, and expected those to
    be empty. In this case where predicates are migrated into union operands, there could
    actually be unassigned conjuncts bound by the SortNode's tuple id (and so would be
    incorrectly picked up). The fix is to not pick up unassigned conjuncts in the SortNode,
    and allow them to be picked up later (into a SelectNode).
    
    Second, when generating the plan for union operands we were missing a call to
    graft a SelectNode on top of the operand plan to capture unassigned conjuncts.
    
    Change-Id: I95d105ac15a3dc975e52dfd418890e13f912dfce
    Reviewed-on: http://gerrit.cloudera.org:8080/3600
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Alex Behm <alex.behm@cloudera.com>
    (cherry picked from commit e6b07f5c3da96f6e89cf5d0583883a7d03c0d718)

--- fe/src/main/java/com/cloudera/impala/planner/SingleNodePlanner.java
+++ fe/src/main/java/com/cloudera/impala/planner/SingleNodePlanner.java
@@ -1590,6 +1590,9 @@ public class SingleNodePlanner {
         }
       }
       PlanNode opPlan = createQueryPlan(queryStmt, op.getAnalyzer(), false);
+      // There may still be unassigned conjuncts if the operand has an order by + limit.
+      // Place them into a SelectNode on top of the operand's plan.
+      opPlan = addUnassignedConjuncts(analyzer, opPlan.getTupleIds(), opPlan);
       if (opPlan instanceof EmptySetNode) continue;
       unionNode.addChild(opPlan, op.getQueryStmt().getBaseTblResultExprs());
     }
