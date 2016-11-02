efc7902beded4c495ef68601b7ec5ea06b3f5580
Alex Behm <alex.behm@cloudera.com>
2016-11-02 10:54:32 -0700

    IMPALA-4423: Correct but conservative implementation of Subquery.equals().
    The underlying problem was for trivial/constant [NOT] EXISTS subqueries
    we substituted out Subqueries with bool literals using an ExprSubstitutionMap,
    but the Subquery.equals() function was not implemented properly, so we ended
    up matching Subqueries to the wrong entry in the ExprSubstitutionMap.
    This could ultimately lead to wrong plans and results.
    
    Testing: Corrected an existing test and modified an existing test for
    extra coverage.
    
    Change-Id: I5562d98ce36507aa5e253323e184fd42b54f27ed
    Reviewed-on: http://gerrit.cloudera.org:8080/4923
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins
    (cherry picked from commit c5f49ec9bbc1b191545b6edd2848acb632b45973)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/19645
    Reviewed-by: Gaal Laszlo <laszlo.gaal@cloudera.com>
    Tested-by: Gaal Laszlo <laszlo.gaal@cloudera.com>

--- fe/src/main/java/com/cloudera/impala/analysis/Subquery.java
+++ fe/src/main/java/com/cloudera/impala/analysis/Subquery.java
@@ -146,6 +146,17 @@ public class Subquery extends Expr {
     return new StructType(structFields);
   }
 
+  /**
+   * Returns true if the toSql() of the Subqueries is identical. May return false for
+   * equivalent statements even due to minor syntactic differences like parenthesis.
+   * TODO: Switch to a less restrictive implementation.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (!super.equals(o)) return false;
+    return stmt_.toSql().equals(((Subquery)o).stmt_.toSql());
+  }
+
   @Override
   public Subquery clone() { return new Subquery(this); }
 
