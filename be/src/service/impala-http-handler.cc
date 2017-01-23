217361c42f8cc5bda64b93122c5b7e1dc5d9eab1
Bharath Vissapragada <bharathv@cloudera.com>
2017-01-22 19:17:38 -0800

    IMPALA-4822: Implement dynamic log level changes
    Very often we have to change the logging levels
    of Impalads and Catalog server for debugging purposes.
    Currently, there is no way to do that without a restart
    of the daemons, which is not a viable option in production
    deployments.
    
    This patch addresses this supportability gap by exposing
    the ability to set dynamic logging levels using a simple
    web endpoint on Impalad/Catalog/Statestore web pages.
    
    This includes setting VLOG levels (equivalent to --v flag)
    as well as setting log4j levels on the Frontend and the
    Catalog JVMs.
    
    Change-Id: I588418e9bcb0b66d33138baf96207a5a35bfbd63
    Reviewed-on: http://gerrit.cloudera.org:8080/5792
    Reviewed-by: Bharath Vissapragada <bharathv@cloudera.com>
    Tested-by: Impala Public Jenkins
    (cherry picked from commit 5d306eff7fa5a0ef8dbccfe6e5135eaeea25ba1a)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/22518
    Reviewed-by: Laszlo Gaal <laszlo.gaal@cloudera.com>
    Tested-by: Laszlo Gaal <laszlo.gaal@cloudera.com>
    (cherry picked from commit 48b5ac5c6ab081dc773b4662ebcc2eb0c5dac73a)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/23332

--- be/src/service/impala-http-handler.cc
+++ be/src/service/impala-http-handler.cc
@@ -29,6 +29,7 @@
 #include "service/query-exec-state.h"
 #include "thrift/protocol/TDebugProtocol.h"
 #include "util/coding-util.h"
+#include "util/logging-support.h"
 #include "util/redactor.h"
 #include "util/summary-util.h"
 #include "util/time.h"
@@ -121,6 +122,8 @@ void ImpalaHttpHandler::RegisterHandlers(Webserver* webserver) {
   webserver->RegisterUrlCallback("/query_stmt", "query_stmt.tmpl",
       [this](const auto& args, auto* doc) {
         this->QuerySummaryHandler(false, false, args, doc); }, false);
+
+  RegisterLogLevelCallbacks(webserver, true);
 }
 
 void ImpalaHttpHandler::HadoopVarzHandler(const Webserver::ArgumentMap& args,
