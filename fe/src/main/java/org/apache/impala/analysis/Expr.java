01186fc7ed5b6372d5eaf070435f86584e8fe8af
Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
2017-08-15 15:27:03 -0700

    IMPALA-5531: Fix correctness issue in correlated aggregate subqueries
    This commit fixes an issue where a query will return wrong results if it
    has an aggregate subquery with a correlated inequality predicate.
    Since the rewrite for this case is not currently supported, an
    exception is now thrown during the analysis.
    
    Change-Id: I6ca7b60ef0543430d2f5a802285254ebb52db2ab
    Reviewed-on: http://gerrit.cloudera.org:8080/7706
    Reviewed-by: Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
    Tested-by: Impala Public Jenkins
    (cherry picked from commit 09c423a235f321e94cddd6e1b7339937b78a7a76)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/26638
    Reviewed-by: Attila Jeges <attilaj@cloudera.com>
    Tested-by: Attila Jeges <attilaj@cloudera.com>

--- fe/src/main/java/org/apache/impala/analysis/Expr.java
+++ fe/src/main/java/org/apache/impala/analysis/Expr.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 
+import org.apache.impala.analysis.BinaryPredicate.Operator;
 import org.apache.impala.analysis.BoolLiteral;
 import org.apache.impala.catalog.Catalog;
 import org.apache.impala.catalog.Function;
@@ -44,6 +45,7 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
@@ -168,6 +170,16 @@ abstract public class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
       new com.google.common.base.Predicate<Expr>() {
         @Override
         public boolean apply(Expr arg) { return BinaryPredicate.getEqSlots(arg) != null; }
+      };
+
+  public final static com.google.common.base.Predicate<Expr> IS_NOT_EQ_BINARY_PREDICATE =
+      new com.google.common.base.Predicate<Expr>() {
+        @Override
+        public boolean apply(Expr arg) {
+          return arg instanceof BinaryPredicate
+              && ((BinaryPredicate) arg).getOp() != Operator.EQ
+              && ((BinaryPredicate) arg).getOp() != Operator.NOT_DISTINCT;
+        }
       };
 
   public final static com.google.common.base.Predicate<Expr> IS_BINARY_PREDICATE =
@@ -1386,5 +1398,20 @@ abstract public class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
       // function calls that return string.
       return DEFAULT_AVG_STRING_LENGTH;
     }
+  }
+
+  /**
+   * Generates a comma-separated string from the toSql() string representations of
+   * 'exprs'.
+   */
+  public static String listToSql(List<Expr> exprs) {
+    com.google.common.base.Function<Expr, String> toSql =
+        new com.google.common.base.Function<Expr, String>() {
+        @Override
+        public String apply(Expr arg) {
+          return arg.toSql();
+        }
+    };
+    return Joiner.on(",").join(Iterables.transform(exprs, toSql));
   }
 }
