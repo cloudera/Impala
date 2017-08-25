0ceac6ed683c8bb62011d1f26909259f0d400839
Jenkins <kitchen-build@cloudera.com>
2017-08-25 13:49:27 -0700

    Update thirdparty dependencies

---
+++ thirdparty/sentry-1.5.1-cdh5.13.1-SNAPSHOT/scripts/sentrystore/upgrade/007-SENTRY-1365.oracle.sql
@@ -0,0 +1,31 @@
+-- Table AUTHZ_PATHS_MAPPING for classes [org.apache.sentry.provider.db.service.model.MAuthzPathsMapping]
+CREATE TABLE AUTHZ_PATHS_MAPPING
+(
+    AUTHZ_OBJ_ID NUMBER NOT NULL,
+    AUTHZ_OBJ_NAME VARCHAR2(384) NULL,
+    CREATE_TIME_MS NUMBER NOT NULL,
+    AUTHZ_SNAPSHOT_ID NUMBER NOT NULL
+);
+
+ALTER TABLE AUTHZ_PATHS_MAPPING ADD CONSTRAINT AUTHZ_PATHS_MAPPING_PK PRIMARY KEY (AUTHZ_OBJ_ID);
+
+-- Constraints for table AUTHZ_PATHS_MAPPING for class(es) [org.apache.sentry.provider.db.service.model.MAuthzPathsMapping]
+CREATE UNIQUE INDEX AUTHZOBJNAME ON AUTHZ_PATHS_MAPPING (AUTHZ_OBJ_NAME);
+
+-- Table `AUTHZ_PATH` for classes [org.apache.sentry.provider.db.service.model.MPath]
+CREATE TABLE AUTHZ_PATH
+ (
+    PATH_ID NUMBER NOT NULL,
+    PATH_NAME VARCHAR(4000),
+    AUTHZ_OBJ_ID NUMBER
+);
+
+-- Constraints for table `AUTHZ_PATH`
+ALTER TABLE AUTHZ_PATH
+  ADD CONSTRAINT AUTHZ_PATH_PK PRIMARY KEY (PATH_ID);
+
+ALTER TABLE AUTHZ_PATH
+  ADD CONSTRAINT AUTHZ_PATH_FK
+  FOREIGN KEY (AUTHZ_OBJ_ID) REFERENCES AUTHZ_PATHS_MAPPING (AUTHZ_OBJ_ID);
+------------------------------------------------------------------
+-- Sequences and SequenceTables
