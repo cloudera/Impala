7b6f847d54ef19bdcf53cdbb8782d9f92bff8009
Zachary Amsden <zamsden@cloudera.com>
2017-10-05 15:40:31 -0700

    CDH-59916: xfail test_data_errors.py
    In C5, HDFS safe mode errors were returned via libHDFS as
    "Unknown Error 255" errors.
    
    In C6, they have a new error code for safe mode errors, which is
    Error(30).  Since the test's primary purpose is to reliably hit a 255
    error, this test has become useless in C6. So, we xfail it now until
    we can find another way of reliably reproducing "Unknown Error 255"
    errors from HDFS in C6.
    
    Change-Id: I28e28ab92e59198fd3631e29cb40d43f8fd8dee3
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/27037
    Reviewed-by: Zachary Amsden <zamsden@cloudera.com>
    Tested-by: Zachary Amsden <zamsden@cloudera.com>

--- tests/data_errors/test_data_errors.py
+++ tests/data_errors/test_data_errors.py
@@ -71,7 +71,7 @@ class TestHdfsFileOpenFailErrors(ImpalaTestSuite):
 class TestHdfsUnknownErrors(ImpalaTestSuite):
   @pytest.mark.execute_serially
   def test_hdfs_safe_mode_error_255(self, unique_database):
-    pytest.xfail("IMPALA-6109: Putting HDFS name node into safe mode trips up HBase")
+    pytest.xfail("CDH-59916: CDH6.x returns Error(30): Read-only file system instead")
     create_stmt = "create table {0}.safe_mode_fail (x int)".format(unique_database)
     insert_stmt = "insert into {0}.safe_mode_fail values (1)".format(unique_database)
     self.execute_query_expect_success(self.client, create_stmt)
