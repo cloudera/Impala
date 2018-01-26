99360675665fb61724fa2f4054e8a2130e6ee46a
Hector Acosta <hector.acosta@cloudera.com>
2018-01-26 12:21:41 -0800

    CLOUDERA-BUILD Add cloudera branching script
    The branching process defaults to performing a string replacement on the
    string 'cdh6.x'. This simple string replacement is not adequate for some
    components; it is in fact not correct for Impala (`git grep cdh6\.x` only
    matches pom comments).
    
    If cloudera/branch.sh exists, it gets executed instead of falling back
    to the process described above. The script is expected to perform any
    required modifications and stage those changes, the rest of the
    branching process will commit and push the changes to $CDH_NEW_BRANCH
    
    For more info about the branching process and other environment
    variables refer to:
    http://github.mtv.cloudera.com/CDH/cdh/blob/cdh6.x/README_cauldron.md#branching-and-branch-names
    
    Testing:
    Commited to my personal fork and branched, then ran a full build here:
    https://master-01.jenkins.cloudera.com/view/Cauldron/job/cauldron_parameterized/3336/
    
    Change-Id: I240c49e05c7406ed1c3a225d0930e92d00a2ea7f
    Signed-off-by: Hector Acosta <hector.acosta@cloudera.com>

---
+++ cloudera/branch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# This script branches Impala in the context of CDH.
+# In c6, the branching process allows components
+# to execute arbitrary scripts instead of falling back to
+# raw string replacement.
+#
+# The branching process expects all modifications to be added
+# to the index, and an exit status of 0 to indicate success.
+#
+# For more info about other environment variables available
+# refer to:
+# http://github.mtv.cloudera.com/CDH/cdh/blob/cdh6.x/README_cauldron.md#branching-and-branch-names
+set -exu
+
+sed -i'' "s/$CDH_START_VERSION/$CDH_NEW_VERSION/g" bin/impala-config-branch.sh
+# Assert something changed and print the diff.
+! git diff --exit-code
+
+git add bin/impala-config-branch.sh
