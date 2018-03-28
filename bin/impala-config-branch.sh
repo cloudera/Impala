eba53877d759fa9a9b8acf0a4b265b55f130440d
Philip Zeyliger <philip@cloudera.com>
2018-03-28 16:32:01 -0700

    CLOUDERA-BUILD: CDH6.x Branch Customizations for Impala
    Some of the versions are inlined to pom.xml to facilitate
    reports that parse the poms. Latest CDH6.x is fetched automatically
    via bin/impala-config-branch.sh
    
    Change-Id: Ib5f198546d78c96721d46755bd37eb7bff7338e4

--- bin/impala-config-branch.sh
+++ bin/impala-config-branch.sh
@@ -22,3 +22,72 @@
 #
 # E.g. to override IMPALA_HADOOP_VERSION, you could uncomment this line:
 # IMPALA_HADOOP_VERSION=3.0.0
+
+# To facilitate building and testing across many possible CDH components,
+# we use $CDH_GBN to specify which build we are working against.
+# This is determined as follows.
+# 1. If $GLOBAL_BUILD_NUMBER is set, use that for $CDH_GBN.
+#    In a Cauldron build, this will be set, as well as DOWNLOAD_CDH_COMPONENTS=false.
+# 2. If toolchain/cdh_components/cdh-gbn.sh exists, source it to set
+#    $CDH_GBN. This will cause subsequent builds in the same $IMPALA_HOME
+#    to continue using the same cdh components.
+# 3. Query BuildDB for the latest CDH build tagged with impala-minicluster-tarballs,
+#    and use that.
+# In addition to being used by bin/bootstrap-toolchain.sh to download the
+# tarballs to support the minicluster, CDH_GBN is used in {fe,testdata}/pom.xml
+# to choose a Maven repository with jars from that specific GBN, though this
+# does not currently always work.
+
+# Defer to $GLOBAL_BUILD_NUMBER OR $CDH_GBN, in that order.
+export CDH_GBN=${GLOBAL_BUILD_NUMBER:-${CDH_GBN:-}}
+VERSION="6.x"
+BRANCH="cdh${VERSION}"
+CDH_GBN_CONFIG="${IMPALA_HOME}/toolchain/cdh_components/cdh-gbn.sh"
+CURL="curl --fail --show-error --silent"
+
+if [ ! "${CDH_GBN}" ]; then
+  if [ -f "${CDH_GBN_CONFIG}" ]; then
+    . "${CDH_GBN_CONFIG}"
+    echo "Using CDH_GBN ${CDH_GBN} based on ${CDH_GBN_CONFIG}"
+  else
+    BUILDDB_QUERY='http://builddb.infra.cloudera.com/query?product=cdh;tag=impala-minicluster-tarballs,official;version='"${VERSION}"
+    export CDH_GBN=$($CURL "${BUILDDB_QUERY}")
+    [ "${CDH_GBN}" ]  # Assert we got something
+    mkdir -p "$(dirname ${CDH_GBN_CONFIG})"
+    echo "export CDH_GBN=${CDH_GBN}" > "${CDH_GBN_CONFIG}"
+    echo "Using CDH_GBN ${CDH_GBN} based on BuildDB query ${BUILDDB_QUERY}."
+  fi
+fi
+
+# Defer to IMPALA_MAVEN_OPTIONS_OVERRIDE. If not set, download an m2-settings.xml file
+# and re-configure to use it.
+if [ ${IMPALA_MAVEN_OPTIONS_OVERRIDE:-} ]; then
+  export IMPALA_MAVEN_OPTIONS=${IMPALA_MAVEN_OPTIONS_OVERRIDE}
+else
+  MAVEN_CONFIG_FILE="${IMPALA_HOME}/toolchain/cdh_components/m2-settings.xml"
+  if [ ! -e "${MAVEN_CONFIG_FILE}" ]; then
+    mkdir -p "$(dirname ${MAVEN_CONFIG_FILE})"
+    $CURL http://github.mtv.cloudera.com/raw/CDH/cdh/${BRANCH}/gbn-m2-settings.xml \
+      -o "${MAVEN_CONFIG_FILE}"
+  fi
+  export IMPALA_MAVEN_OPTIONS="-s ${MAVEN_CONFIG_FILE}"
+fi
+
+BUILD_REPO_BASE="http://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/${CDH_GBN}/impala-minicluster-tarballs"
+
+# Note that the Maven/Java version strings are also in impala-parent/pom.xml,
+# and version changes need to be reflected there as well. The tarballs are
+# versioned as -<GBN> whereas the Maven versions do not include a GBN, but
+# include a -SNAPSHOT (as appropriate).
+export IMPALA_HADOOP_VERSION=3.0.0-${BRANCH}-${CDH_GBN}
+export IMPALA_HADOOP_URL=${BUILD_REPO_BASE}/hadoop-${IMPALA_HADOOP_VERSION}.tar.gz
+export IMPALA_HBASE_VERSION=2.0.0-${BRANCH}-${CDH_GBN}
+export IMPALA_HBASE_URL=${BUILD_REPO_BASE}/hbase-${IMPALA_HBASE_VERSION}.tar.gz
+export IMPALA_HIVE_VERSION=2.1.1-${BRANCH}-${CDH_GBN}
+export IMPALA_HIVE_URL=${BUILD_REPO_BASE}/hive-${IMPALA_HIVE_VERSION}.tar.gz
+export IMPALA_SENTRY_VERSION=2.0.0-${BRANCH}-${CDH_GBN}
+export IMPALA_SENTRY_URL=${BUILD_REPO_BASE}/sentry-${IMPALA_SENTRY_VERSION}.tar.gz
+export IMPALA_PARQUET_VERSION=1.9.0-${BRANCH}
+export IMPALA_AVRO_JAVA_VERSION=1.8.2-${BRANCH}
+export KUDU_JAVA_VERSION=1.8.0-${BRANCH}-SNAPSHOT
+export IMPALA_KITE_VERSION=1.0.0-${BRANCH}-SNAPSHOT
