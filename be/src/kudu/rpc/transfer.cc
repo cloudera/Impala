a3b6374c73c8ec00b969b02cff782657e6236df6
Philip Zeyliger <philip@cloudera.com>
2018-05-04 13:11:13 -0700

    Revert "KUDU-2334: Fix OutboundTransfer::TransferStarted() to work with SSL_write()"
    This reverts commit 8079cd9d2a87051f81a41910b74fab15e35f36ea.

--- be/src/kudu/rpc/transfer.cc
+++ be/src/kudu/rpc/transfer.cc
@@ -159,7 +159,6 @@ OutboundTransfer::OutboundTransfer(int32_t call_id,
     cur_offset_in_slice_(0),
     callbacks_(callbacks),
     call_id_(call_id),
-    started_(false),
     aborted_(false) {
 
   n_payload_slices_ = n_payload_slices;
@@ -186,7 +185,6 @@ void OutboundTransfer::Abort(const Status &status) {
 Status OutboundTransfer::SendBuffer(Socket &socket) {
   CHECK_LT(cur_slice_idx_, n_payload_slices_);
 
-  started_ = true;
   int n_iovecs = n_payload_slices_ - cur_slice_idx_;
   struct iovec iovec[n_iovecs];
   {
@@ -234,7 +232,7 @@ Status OutboundTransfer::SendBuffer(Socket &socket) {
 }
 
 bool OutboundTransfer::TransferStarted() const {
-  return started_;
+  return cur_offset_in_slice_ != 0 || cur_slice_idx_ != 0;
 }
 
 bool OutboundTransfer::TransferFinished() const {
