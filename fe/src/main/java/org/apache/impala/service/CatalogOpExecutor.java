73a91a8252744006c813ddf95cee1c8c6b318dcc
Todd Lipcon <todd@cloudera.com>
2018-06-06 10:30:34 -0700

    IMPALA-7128 (part 2): add an interface for data sources
    This changes most of the usage of DataSource and DataSourceTable to use
    interfaces instead of implementation classes. There are still various
    usages of the implementation for functionality like creating and
    dropping data sources. We'll address those as part of IMPALA-7131 at a
    later date.
    
                ==C5_FEATURE_IMPALA_METADATA==
    
    Change-Id: Ibe704197dc2ad7c09b8340865f17567096aa630e
    Reviewed-on: http://gerrit.cloudera.org:8080/10626
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    (cherry picked from commit 1b23c2b8d9640cd7ff8eb924eb427715323ecabe)

--- fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
+++ fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
@@ -1075,6 +1075,7 @@ public class CatalogOpExecutor {
 
   private void createDataSource(TCreateDataSourceParams params, TDdlExecResponse resp)
       throws ImpalaException {
+    // TODO(IMPALA-7131): support data sources with LocalCatalog.
     if (LOG.isTraceEnabled()) { LOG.trace("Adding DATA SOURCE: " + params.toString()); }
     DataSource dataSource = DataSource.fromThrift(params.getData_source());
     DataSource existingDataSource = catalog_.getDataSource(dataSource.getName());
@@ -1094,6 +1095,7 @@ public class CatalogOpExecutor {
 
   private void dropDataSource(TDropDataSourceParams params, TDdlExecResponse resp)
       throws ImpalaException {
+    // TODO(IMPALA-7131): support data sources with LocalCatalog.
     if (LOG.isTraceEnabled()) LOG.trace("Drop DATA SOURCE: " + params.toString());
     DataSource dataSource = catalog_.removeDataSource(params.getData_source());
     if (dataSource == null) {
