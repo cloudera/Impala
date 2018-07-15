fe072303495a3b200f7421aa9e1584cff60e0cbb
Fredy Wijaya <fwijaya@cloudera.com>
2018-07-15 10:45:06 -0700

    Revert "Revert "IMPALA-6988: Implement ALTER TABLE/VIEW SET OWNER""
    This reverts commit c01efd09679faaacfd5488fc7f4c1526a1af2f35.
    
    Change-Id: I47ed9c6bb983120afdafadea8b65a9239cbf6d0f
    Reviewed-on: http://gerrit.cloudera.org:8080/10947
    Reviewed-by: Fredy Wijaya <fwijaya@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    (cherry picked from commit 8c276217670e1ddee1224c982bc429f49c7843bc)
    
            ==C5_FEATURE_FGP==

--- fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
+++ fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
@@ -98,6 +98,7 @@ import org.apache.impala.thrift.TAlterTableAddReplaceColsParams;
 import org.apache.impala.thrift.TAlterTableAlterColParams;
 import org.apache.impala.thrift.TAlterTableDropColParams;
 import org.apache.impala.thrift.TAlterTableDropPartitionParams;
+import org.apache.impala.thrift.TAlterTableOrViewSetOwnerParams;
 import org.apache.impala.thrift.TAlterTableParams;
 import org.apache.impala.thrift.TAlterTableSetCachedParams;
 import org.apache.impala.thrift.TAlterTableSetFileFormatParams;
@@ -531,6 +532,11 @@ public class CatalogOpExecutor {
           break;
         case RECOVER_PARTITIONS:
           alterTableRecoverPartitions(tbl);
+          break;
+        case SET_OWNER:
+          Preconditions.checkState(params.isSetSet_owner_params());
+          alterTableOrViewSetOwner(tbl, params.getSet_owner_params());
+          addSummary(response, "Updated table/view.");
           break;
         default:
           throw new UnsupportedOperationException(
@@ -2723,6 +2729,14 @@ public class CatalogOpExecutor {
     }
   }
 
+  private void alterTableOrViewSetOwner(Table tbl, TAlterTableOrViewSetOwnerParams params)
+      throws ImpalaRuntimeException {
+    org.apache.hadoop.hive.metastore.api.Table msTbl = tbl.getMetaStoreTable().deepCopy();
+    msTbl.setOwner(params.owner_name);
+    msTbl.setOwnerType(PrincipalType.valueOf(params.owner_type.name()));
+    applyAlterTable(msTbl, true);
+  }
+
   /**
    * Create a new HMS Partition.
    */
@@ -3472,7 +3486,7 @@ public class CatalogOpExecutor {
       }
     }
     addDbToCatalogUpdate(db, response.result);
-    addSummary(response, "Updated database");
+    addSummary(response, "Updated database.");
   }
 
   private void addDbToCatalogUpdate(Db db, TCatalogUpdateResult result) {
