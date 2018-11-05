3a1489fb9bf68c39dbc47278fbc63d41a78ff58e
Laszlo Gaal <laszlo.gaal@cloudera.com>
2018-11-05 17:48:16 +0100

    CDH-74307: IMPALA-7537: REVOKE GRANT OPTION regression (partial backport)
    Revoking privileges on SERVER did not pass NULL for the grantoption flag,
    breaking the revoke logic.
    The original fix, IMPALA-7537 fixes and adds a lot more than just this
    problem, which makes it incompatible with CDH5.15.x. This selective
    backport takes only the point fix of passing in the NULL parameter.
    The backport omits the test framework, so the patch was verified manually
    on a Sentry-enabled minicluster.
    
    Verification steps in impala-shell:
    
    create role foo;
    grant all on server to role foo with grant option;
    revoke all on server from role foo;
    show grant role foo; -- The privilege should not show here.
    invalidate metadata;
    show grant role foo; -- The privilege should still not show.
    
      ==C5_APPROVED_BUGFIX==
    
    Change-Id: I6fdbacf95aae2f667ab120b53a2842a797326998
    Reviewed-on: https://gerrit.sjc.cloudera.com/39170
    Reviewed-by: Laszlo Gaal <laszlo.gaal@cloudera.com>
    Tested-by: Laszlo Gaal <laszlo.gaal@cloudera.com>

--- fe/src/main/java/org/apache/impala/util/SentryPolicyService.java
+++ fe/src/main/java/org/apache/impala/util/SentryPolicyService.java
@@ -330,7 +330,8 @@ public class SentryPolicyService {
       switch (scope) {
         case SERVER:
           client.get().revokeServerPrivilege(requestingUser.getShortName(), roleName,
-              privilege.getServer_name(), privilege.getPrivilege_level().toString());
+              privilege.getServer_name(), privilege.getPrivilege_level().toString(), 
+              null);
           break;
         case DATABASE:
           client.get().revokeDatabasePrivilege(requestingUser.getShortName(), roleName,
