a178723a548a42985f382acc60810b26d4f98efa
Vihang Karajgaonkar <vihang@cloudera.com>
2019-03-01 18:16:07 -0800

    IMPALA-8273 : Change metastore config template to not exclude impala specific parameters
    When HMS configuration key
    "hive.metastore.notification.parameters.exclude.patterns"
    is set to "^impala" it excludes all the parameter key/values from the
    table and partition which start with "impala". This can cause problems
    for event processing since it relies on these property keys
    (for instance, impala.disableHmsSync).
    
    Testing done:
    Confirmed existing test pass.
    Ran jenkins job which runs all the fe tests where the events disable
    flag tests were failing from MetastoreEventsProcessingTest class.
    Job currently running. Will update the message once the result
    comes back.
    
    Change-Id: Ic367b1d06459295c7721cdb20ac7faed9a2aa4c9
    Reviewed-on: http://gerrit.cloudera.org:8080/12646
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Reviewed-on: https://gerrit.sjc.cloudera.com/42426
    Tested-by: Jenkins User <jenkins@cloudera.com>
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Quasar-L0: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Vihang Karajgaonkar <vihang@cloudera.com>
    Reviewed-by: Adrian Ng <adrian.ng@cloudera.com>

--- fe/src/test/java/org/apache/impala/catalog/events/MetastoreEventsProcessorTest.java
+++ fe/src/test/java/org/apache/impala/catalog/events/MetastoreEventsProcessorTest.java
@@ -99,6 +99,8 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Main test class to cover the functionality of MetastoreEventProcessor. In order to make
@@ -118,6 +120,8 @@ public class MetastoreEventsProcessorTest {
   private static CatalogServiceCatalog catalog_;
   private static CatalogOpExecutor catalogOpExecutor_;
   private static MetastoreEventsProcessor eventsProcessor_;
+  private static final Logger LOG =
+      LoggerFactory.getLogger(MetastoreEventsProcessorTest.class);
 
   @BeforeClass
   public static void setUpTestEnvironment() throws TException {
@@ -968,7 +972,7 @@ public class MetastoreEventsProcessorTest {
 
     AlterTableEvent alterTableEvent = new AlterTableEvent(
         fakeCatalog, eventsProcessor_.getMetrics(), fakeAlterTableNotification);
-    Assert.assertFalse("Alter table which " + "changes the flags should not be skipped. "
+    Assert.assertFalse("Alter table which changes the flags should not be skipped. "
             + printFlagTransistions(dbFlag, tblFlagTransition),
         alterTableEvent.isEventProcessingDisabled());
 
