3d87e212c72d10e4e9118237b417f50dcd896513
Andrew Sherman <asherman@cloudera.com>
2019-05-23 09:58:00 -0700

    IMPALA-8333: Remove Impala Shell warnings part 2
    Set IMPALA_TOOLCHAIN_BUILD_ID=40-193a30b3af to pickup "Patch Thrift to
    0.9.3-p6 to eliminate ssl warnings"
    
    Set IMPALA_THRIFT_VERSION=0.9.3-p6 to pick up the new thrift build,
    which removes an unnecessary and confusing warning.
    
    TESTING
    
    Change the tests in test_client_ssl.py which were looking for specific
    deprecation warnings to instead search for any Deprecation Warning.
    Ran all end-to-end tests with new toolchain.
    Built Impala on all supported platforms.
    
    6.2.1 merging: changed some version in bin/impala-config.sh
    
    Change-Id: I8ae7e068894da5981fc083e690051da268bfde4d
    Reviewed-on: http://gerrit.cloudera.org:8080/13404
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    (cherry picked from commit 7b9d65928234aa09447daac807a0089c965bb0a1)
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/55472
    Reviewed-by: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    Tested-by: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Andrew Sherman <asherman@cloudera.com>
    CDH-Build: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    Quasar-L0: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    
    Change-Id: I45c5447c9b6acd4e3cbc95f4647146e7f0ce31f7
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/62210
    Tested-by: Jenkins User <jenkins@cloudera.com>
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Quasar-L0: Zsolt Fekete <zfekete@cloudera.com>
    Reviewed-by: Laszlo Gaal <laszlo.gaal@cloudera.com>
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/62857
    CDH-Build: Zsolt Fekete <zfekete@cloudera.com>
    Reviewed-by: Zsolt Fekete <zfekete@cloudera.com>
    Tested-by: Zsolt Fekete <zfekete@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -68,7 +68,7 @@ fi
 # moving to a different build of the toolchain, e.g. when a version is bumped or a
 # compile option is changed. The build id can be found in the output of the toolchain
 # build jobs, it is constructed from the build number and toolchain git hash prefix.
-export IMPALA_TOOLCHAIN_BUILD_ID=24-3b615798c1
+export IMPALA_TOOLCHAIN_BUILD_ID=40-193a30b3af
 # Versions of toolchain dependencies.
 # -----------------------------------
 export IMPALA_AVRO_VERSION=1.7.4-p4
@@ -83,7 +83,7 @@ export IMPALA_BZIP2_VERSION=1.0.6-p2
 unset IMPALA_BZIP2_URL
 export IMPALA_CCTZ_VERSION=2.2
 unset IMPALA_CCTZ_URL
-export IMPALA_CMAKE_VERSION=3.8.2-p1
+export IMPALA_CMAKE_VERSION=3.14.3
 unset IMPALA_CMAKE_URL
 export IMPALA_CRCUTIL_VERSION=440ba7babeff77ffad992df3a10c767f184e946e-p1
 unset IMPALA_CRCUTIL_URL
@@ -143,7 +143,7 @@ export IMPALA_TPC_DS_VERSION=2.1.0
 unset IMPALA_TPC_DS_URL
 export IMPALA_TPC_H_VERSION=2.17.0
 unset IMPALA_TPC_H_URL
-export IMPALA_THRIFT_VERSION=0.9.3-p5
+export IMPALA_THRIFT_VERSION=0.9.3-p6
 unset IMPALA_THRIFT_URL
 export IMPALA_THRIFT11_VERSION=0.11.0-p2
 unset IMPALA_THRIFT11_URL
