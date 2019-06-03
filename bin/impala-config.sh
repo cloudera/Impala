7d1446c185dc370a14306141ca592f4cdc852262
Hector Acosta <hector.acosta@cloudera.com>
2019-06-03 15:16:00 -0700

    RELENG-5671 Bump toolchain version
    The base docker images used to build cdh6.3 are currently newer than the
    images used to build the native-toolchain. The toolchain for Impala needs
    to be built on these newer images to be compatible. This commit resets
    the toolchain build id to the first version that used compatible workers.
    
    Change-Id: Icf801ae3b26ceb1ac06c8b7dd10aa7d8acd09fce
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/47980
    Tested-by: Hector Acosta <hector.acosta@cloudera.com>
    API-Compat: Hector Acosta <hector.acosta@cloudera.com>
    CDH-Build: Hector Acosta <hector.acosta@cloudera.com>
    Reviewed-by: Hector Acosta <hector.acosta@cloudera.com>
    Quasar-L0: Hector Acosta <hector.acosta@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -68,7 +68,7 @@ fi
 # moving to a different build of the toolchain, e.g. when a version is bumped or a
 # compile option is changed. The build id can be found in the output of the toolchain
 # build jobs, it is constructed from the build number and toolchain git hash prefix.
-export IMPALA_TOOLCHAIN_BUILD_ID=43961c5c-7ece-489c-a6e3-ec4cbe7ef4b5-b23c19a002
+export IMPALA_TOOLCHAIN_BUILD_ID=16-2402d830d5
 # Versions of toolchain dependencies.
 # -----------------------------------
 export IMPALA_AVRO_VERSION=1.7.4-p4
@@ -122,7 +122,7 @@ export IMPALA_OPENLDAP_VERSION=2.4.47
 unset IMPALA_OPENLDAP_URL
 export IMPALA_OPENSSL_VERSION=1.0.2l
 unset IMPALA_OPENSSL_URL
-export IMPALA_ORC_VERSION=1.4.3-p3
+export IMPALA_ORC_VERSION=1.5.5-p1
 unset IMPALA_ORC_URL
 export IMPALA_PROTOBUF_VERSION=3.5.1
 unset IMPALA_PROTOBUF_URL
@@ -130,7 +130,7 @@ export IMPALA_POSTGRES_JDBC_DRIVER_VERSION=9.0-801
 unset IMPALA_POSTGRES_JDBC_DRIVER_URL
 export IMPALA_RAPIDJSON_VERSION=1.1.0
 unset IMPALA_RAPIDJSON_URL
-export IMPALA_RE2_VERSION=20130115-p1
+export IMPALA_RE2_VERSION=20190301
 unset IMPALA_RE2_URL
 export IMPALA_SNAPPY_VERSION=1.1.4
 unset IMPALA_SNAPPY_URL
