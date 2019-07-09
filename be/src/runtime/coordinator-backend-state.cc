65ccd6ad4bbf1a9c302c979127bf5711a491ae93
Michael Ho <kwho@cloudera.com>
2019-07-08 17:28:41 -0700

    IMPALA-8748: Always pass hostname to RpcMgr::GetProxy()
    This change fixes some callers of RpcMgr::GetProxy() tp
    pass the hostname instead of the resolved IP adddress.
    The hostname passed to RpcMgr::GetProxy() is used to
    construct the principal name of the remote destination
    when Kerberos is enabled.
    
    Change-Id: I85b661c8c3b3b67bfc1ce9e29aecb90a862666c0
    Reviewed-on: http://gerrit.cloudera.org:8080/13818
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    (cherry picked from commit 107dd542bd371a501579ca61504c3cb6736bf34f)
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/55476
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    Reviewed-by: Michael Ho <kwho@cloudera.com>
    Quasar-L0: quasar-precommit User
    Tested-by: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    
    Change-Id: I4b2880af7c6aa3cb6a721c5c717b81e53b6f81c1
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/62205
    Tested-by: Jenkins User <jenkins@cloudera.com>
    Quasar-L0: Zsolt Fekete <zfekete@cloudera.com>
    CDH-Build: Zsolt Fekete <zfekete@cloudera.com>
    Reviewed-by: Laszlo Gaal <laszlo.gaal@cloudera.com>
    Reviewed-by: Zsolt Fekete <zfekete@cloudera.com>
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/62852
    Tested-by: Zsolt Fekete <zfekete@cloudera.com>

--- be/src/runtime/coordinator-backend-state.cc
+++ be/src/runtime/coordinator-backend-state.cc
@@ -446,8 +446,7 @@ bool Coordinator::BackendState::Cancel() {
              << " backend=" << TNetworkAddressToString(krpc_host_);
 
   std::unique_ptr<ControlServiceProxy> proxy;
-  Status get_proxy_status =
-      ControlService::GetProxy(krpc_host_, krpc_host_.hostname, &proxy);
+  Status get_proxy_status = ControlService::GetProxy(krpc_host_, host_.hostname, &proxy);
   if (!get_proxy_status.ok()) {
     status_.MergeStatus(get_proxy_status);
     VLOG_QUERY << "Cancel query_id= " << PrintId(query_id()) << " could not get proxy to "
